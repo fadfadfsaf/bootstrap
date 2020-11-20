cd2c71952ebce169457d1443f49ac7bfd04fcc8c
lyx <1092642896@qq.com>
2020-11-20 14:32:47 +0800

    ak

--- cms/爱快cms/js/login.js
+++ cms/爱快cms/js/login.js
@@ -1,7 +1,9 @@
 $(function () {
-    $("input[name=btn]").click(function () {
+
+    $("input[name=btn]").on("click", function () {
         var names = $("input[name=username]").val();
         var pwd = $("input[name=userpwd]").val();
+        console.log("hello world");
         $.ajax({
             url: "http://www.qhdlink-student.top/student/login.php",
             type: "post",
@@ -10,13 +12,72 @@ $(function () {
             },
             success: function (data) {
                 console.log(data);
-                if (data == "ok") {
-                    localStorage.setItem("linkname",names);
-                    location.href = "index.html";
+                if(data="ok"){
+                    location.href="index.html";
                 }else{
-                    alert("用户名或密码错误")
+                    alert("账号或密码不正确")
                 }
             }
         })
+        // location.href="index.html";
     })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function () {
+//     $("input[name=btn]").click(function () {
+//         var names = $("input[name=username]").val();
+//         var pwd = $("input[name=userpwd]").val();
+//         $.ajax({
+//             url: "http://www.qhdlink-student.top/student/login.php",
+//             type: "post",
+//             data: {
+//                 "username": names, "userpwd": pwd, "userclass": 61, "type": 2
+//             },
+//             success: function (data) {
+//                 console.log(data);
+//                 if (data == "ok") {
+//                     localStorage.setItem("linkname",names);
+//                     location.href = "index.html";
+//                 }else{
+//                     alert("用户名或密码错误")
+//                 }
+//             }
+//         })
+//     })
+// })
