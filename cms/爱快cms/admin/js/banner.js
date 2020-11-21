77bd2c2865fb837cbd533d1e7a976bbc65097e3c
lyx <1092642896@qq.com>
2020-11-21 22:20:58 +0800

    1

--- cms/爱快cms/admin/js/banner.js
+++ cms/爱快cms/admin/js/banner.js
@@ -4,18 +4,27 @@ $(function () {
         type: "post",
         typeData: "json",
         success: function (obj) {
-            console.log(obj);
+            // console.log(obj);
             var data = obj.data;
-            console.log(data);
+            // console.log(data);
             var str = '';
             $.each(data, function (i, v) {
-                console.log(v);
+                // console.log(v);
                 str += '<p>id：' + v.id_b + ';<span> name :' + v.name_b + '</span><img src="' + v.img_b + '"><button class="but0">修改</button><button class="but1">删除</button></p>'
             })
             $("#msg").append($(str));
+
+
             $(".but0").click(function () {
-                alert("hello")
+                var img = $(this).prev();
+                // img.remove();
+                console.log(img[0].src);
+                // var fi= $("<input type='file'>")
+                // console.log(fi);
+                   img[0].src='';
+
             })
+
             $(".but1").click(function () {
                 var r = confirm("是否确认删除");
                 var x;
@@ -31,43 +40,45 @@ $(function () {
         }
     })
 
-    var a=2;
+    var a = 2;
     $(".add").change(function () {
         var file = this.files[0];
-        var imgs = $("<img>");   
+        var imgs = $("<img>");
         if (window.FileReader) {
             var fr = new FileReader();
             fr.readAsDataURL(file);
 
             fr.onload = function () {
                 console.log(this);
-                imgs.attr("src",this.result); // 图片可显示出来
-                
+                imgs.attr("src", this.result); // 图片可显示出来
+
             };
 
             $("#add").one("click", function () {
                 a++;
-                alert("添加成功")
-                var ps=$("<p></p>");
-                ps.append('id: '+a+'<span>图片名称</span>')
+
+                var ps = $("<p></p>");
+                ps.append('id: ' + a + '<span>图片名称</span>')
                 ps.append(imgs);
-                ps.append('<button class="but0">修改</button><button class="but1">删除</button>');
+                ps.append('<button class="but2">修改</button><button class="but3">删除</button>')
                 $("#msg").append(ps);
-            })
-            $(".but0").click(function () {
-                alert("hello")
-            })
-            $(".but1").click(function () {
-                var r = confirm("是否确认删除");
-                var x;
-                if (r == true) {
-                    x = "你按下了\"确定\"按钮!";
-                    $(this).parent().remove();
-                }
-                else {
-                    x = "你按下了\"取消\"按钮!";
-                }
-                console.log(confirm);
+                alert("添加成功")
+
+                $(".but2").on("click", function () {
+                    alert(this);
+                })
+                $(".but3").one("click", function () {
+                    var r = confirm("是否确认删除");
+                    var x;
+                    if (r == true) {
+                        x = "你按下了\"确定\"按钮!";
+                        $(this).parent().remove();
+                    }
+                    else {
+                        x = "你按下了\"取消\"按钮!";
+                    }
+                    console.log(confirm);
+                })
             })
 
         } else {
