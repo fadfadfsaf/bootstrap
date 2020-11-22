517bb7da39ec986790139d83582968a9ae182876
lyx <1092642896@qq.com>
2020-11-22 22:54:05 +0800

    阿萨德

--- bootstrap/腾润驾培/tengrun/lessonPrice/js/lessonPrice.js
+++ bootstrap/腾润驾培/tengrun/lessonPrice/js/lessonPrice.js
@@ -29,34 +29,43 @@ $(function () {
         },
         success: function (data) {
             console.log(data);
-            // for (var i = 0; i < Object.keys(data).length; i++) {
+            // each遍历data数据
             $.each(data, function (i, v) {
                 console.log(i);
                 var str1 = '';
                 var str = '';
+                // i是data内容下标，每三个内容称为一个分界点;添加一个div类名为right
                 if (i % 3 == 0) {
                     str1 += '<div class="right"></div>';
                     $(".contentses").append($(str1));
 
                     str += '<div class="right_1"><div class="right_img"><img src="http://www.qhdlink-student.top/' + data[i].path_coach + '"></div><div class="right_text"><h3>C1</h3><p>精英班/8:00~12:00 13:00~17:00</p><a href="">详细了解>></a></div><div class="right_button"><a href="" class="sign">网上报名</a></div></div>';
                     $(".right:last-child").append($(str));
+                    // 否则只添加内容，不添加div节点
                 } else {
                     str += '<div class="right_1"><div class="right_img"><img src="http://www.qhdlink-student.top/' + data[i].path_coach + '"></div><div class="right_text"><h3>C1</h3><p>精英班/8:00~12:00 13:00~17:00</p><a href="">详细了解>></a></div><div class="right_button"><a href="" class="sign">网上报名</a></div></div>';
                     $(".right:last-child").append($(str));
                 }
             })
+            // 找到right节点。遍历它并且用下标作为页码，添加到ul里
             for (var j = 1; j <= $(".right").length; j++) {
                 var str2 = '';
                 str2 += '<li>' + j + '</li>';
                 $(".ul").append($(str2));
             }
 
-
+            // 所有的right页面都隐藏（css设置）
+            // 设置第一个right显示出来
             $(".right").eq(0).show()
 
+            // 点击页码事件，tab切换效果
             $(".ul>li").on("click", function () {
+                // 找到每个li的下标
                 var index = $(this).index();
-                $(this).css({"background":"#fff","color":"#FF9914"}).siblings().css({"background":"#FF9914","color":"#fff"});
+                // 它的背景颜色和字体颜色发生变化
+                // 他的兄弟背景颜色和字体颜色变化
+                $(this).css({ "background": "#fff", "color": "#FF9914" }).siblings().css({ "background": "#FF9914", "color": "#fff" });
+                // 点击哪个li。下标为几的right显示。其他right消失
                 $(".right").eq(index).show().siblings().hide();
             })
         }
